93678043ca34e38293f9804b8fceb5b2c1859f61
Acittt <rasyiidraafi05@gmail.com>
2025-03-02 13:09:34 +0700

    feat: add animation on buttom navbar

--- app/src/main/java/com/example/opendatajabar/OpenDataJabar.kt
+++ app/src/main/java/com/example/opendatajabar/OpenDataJabar.kt
@@ -1,24 +1,22 @@
 package com.example.opendatajabar
 
+import androidx.compose.animation.*
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
-import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.List
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.ui.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavType
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.currentBackStackEntryAsState
-import androidx.navigation.compose.rememberNavController
+import androidx.navigation.compose.*
 import androidx.navigation.navArgument
 import com.example.opendatajabar.ui.navigation.NavigationItem
 import com.example.opendatajabar.ui.navigation.Screen
@@ -31,6 +29,7 @@ import com.example.opendatajabar.ui.theme.OpenDataJabarTheme
 import com.example.opendatajabar.viewmodel.DataViewModel
 import com.example.opendatajabar.viewmodel.ProfileViewModel
 
+@OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun DataOpenJabarApp(
     modifier: Modifier = Modifier,
@@ -39,7 +38,7 @@ fun DataOpenJabarApp(
     profileViewModel: ProfileViewModel = viewModel()
 ) {
     val navBackStackEntry by navController.currentBackStackEntryAsState()
-    val currentRoute = navBackStackEntry?.destination?.route
+    val currentRoute = navBackStackEntry?.destination?.route ?: Screen.Home.route
 
     Scaffold(
         bottomBar = {
@@ -49,28 +48,46 @@ fun DataOpenJabarApp(
         },
         modifier = modifier
     ) { innerPadding ->
-        NavHost(
-            navController = navController,
-            startDestination = Screen.Home.route,
-            modifier = Modifier.padding(innerPadding)
-        ) {
-            composable(Screen.DataEntry.route) {
-                DataEntryScreen(viewModel = viewModel)
-            }
-            composable(Screen.Edit.route,
-                arguments = listOf(navArgument("id") { type = NavType.IntType }))
-            { backStackEntry ->
-                val id = backStackEntry.arguments?.getInt("id") ?: 0
-                EditScreen(navController = navController, viewModel = viewModel, dataId = id)
-            }
-            composable(Screen.DataList.route) {
-                DataListScreen(navController = navController, viewModel = viewModel)
-            }
-            composable(Screen.Profile.route) {
-                ProfileScreen(viewModel = profileViewModel)
-            }
-            composable(Screen.Home.route){
-                HomeScreen(viewModel = viewModel)
+        AnimatedContent(
+            targetState = currentRoute,
+            transitionSpec = {
+                slideInHorizontally(
+                    initialOffsetX = { fullWidth -> fullWidth }, // Geser dari kanan
+                    animationSpec = tween(500)
+                ) with slideOutHorizontally(
+                    targetOffsetX = { fullWidth -> -fullWidth }, // Geser ke kiri
+                    animationSpec = tween(500)
+                )
+            },
+            label = "ScreenTransition"
+        ) { targetScreen ->
+            NavHost(
+                navController = navController,
+                startDestination = Screen.Home.route,
+                modifier = Modifier.padding(innerPadding)
+            ) {
+                composable(Screen.Home.route) {
+                    if (targetScreen == Screen.Home.route) HomeScreen(viewModel = viewModel)
+                }
+                composable(Screen.DataEntry.route) {
+                    if (targetScreen == Screen.DataEntry.route) DataEntryScreen(viewModel = viewModel)
+                }
+                composable(Screen.Edit.route,
+                    arguments = listOf(navArgument("id") { type = NavType.IntType })
+                ) { backStackEntry ->
+                    val id = backStackEntry.arguments?.getInt("id") ?: 0
+                    if (targetScreen == Screen.Edit.route) {
+                        EditScreen(navController = navController, viewModel = viewModel, dataId = id)
+                    }
+                }
+                composable(Screen.DataList.route) {
+                    if (targetScreen == Screen.DataList.route) {
+                        DataListScreen(navController = navController, viewModel = viewModel)
+                    }
+                }
+                composable(Screen.Profile.route) {
+                    if (targetScreen == Screen.Profile.route) ProfileScreen(viewModel = profileViewModel)
+                }
             }
         }
     }
@@ -87,42 +104,22 @@ private fun BottomBar(
         val navBackStackEntry by navController.currentBackStackEntryAsState()
         val currentRoute = navBackStackEntry?.destination?.route
         val navigationItems = listOf(
-            NavigationItem(
-                title = "Home",
-                icon = Icons.Default.Home,
-                screen = Screen.Home
-            ),
-            NavigationItem(
-                title = "Entry Data",
-                icon = Icons.Default.Add,
-                screen = Screen.DataEntry
-            ),
-            NavigationItem(
-                title = "List",
-                icon = Icons.Default.List,
-                screen = Screen.DataList
-            ),
-            NavigationItem(
-                title = "Profile",
-                icon = Icons.Default.AccountCircle,
-                screen = Screen.Profile
-            ),
+            NavigationItem("Home", Icons.Default.Home, Screen.Home),
+            NavigationItem("Entry Data", Icons.Default.Add, Screen.DataEntry),
+            NavigationItem("List", Icons.Default.List, Screen.DataList),
+            NavigationItem("Profile", Icons.Default.AccountCircle, Screen.Profile),
         )
-        navigationItems.map { item ->
+
+        navigationItems.forEach { item ->
+            val isSelected = currentRoute == item.screen.route
+
             NavigationBarItem(
-                icon = {
-                    Icon(
-                        imageVector = item.icon,
-                        contentDescription = item.title
-                    )
-                },
-                label = { Text(item.title) },
-                selected = currentRoute == item.screen.route,
+                icon = { Icon(item.icon, contentDescription = item.title) },
+                label = { if (isSelected) Text(item.title) },
+                selected = isSelected,
                 onClick = {
                     navController.navigate(item.screen.route) {
-                        popUpTo(navController.graph.findStartDestination().id) {
-                            saveState = true
-                        }
+                        popUpTo(navController.graph.findStartDestination().id) { saveState = true }
                         restoreState = true
                         launchSingleTop = true
                     }
@@ -139,10 +136,8 @@ private fun BottomBar(
 
 @Preview(showBackground = true)
 @Composable
-fun opendatajabarAPPPreview() {
+fun OpenDataJabarPreview() {
     OpenDataJabarTheme {
-        DataOpenJabarApp(
-            viewModel = viewModel()
-        )
+        DataOpenJabarApp(viewModel = viewModel())
     }
 }
